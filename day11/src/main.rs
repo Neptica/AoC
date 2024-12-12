12af1a8fa06bc49b6a7e8620314c121b91008b18
Michael Mumme <neptica@hotmail.com>
2024-12-11 22:14:51 -0600

    day11 pt2 not quite

--- day11/src/main.rs
+++ day11/src/main.rs
@@ -11,30 +11,6 @@ where
     Ok(io::BufReader::new(file).lines())
 }
 
-fn action(mut rock: String, depth: u32) -> u128 {
-    if depth >= 75 {
-        return 1;
-    }
-    // print!("{} ", rock);
-
-    if rock.len() % 2 == 0 {
-        let middle = rock.len() / 2;
-        let second_half = rock[middle..]
-            .parse::<u64>()
-            .expect("Bad Parse")
-            .to_string(); // to
-                          // remove leading zeros in the second value
-        let first_half = rock[..middle].to_string();
-        action(first_half, depth + 1) + action(second_half, depth + 1)
-    } else if rock == "0" {
-        rock = "1".to_string();
-        return action(rock, depth + 1);
-    } else {
-        rock = (rock.parse::<u128>().expect("Parse error") * 2024).to_string();
-        return action(rock, depth + 1);
-    }
-}
-
 fn main() {
     let input = read_file("./input.txt")
         .expect("Failed to read")
@@ -42,17 +18,80 @@ fn main() {
         .expect("Failed to get input")
         .expect("Failed to get line");
 
-    let line: Vec<String> = input
+    let mut line: Vec<(u128, u64)> = input
         .split(" ")
-        .map(|str| str.to_string())
-        .collect::<Vec<String>>();
+        .map(|str| (str.parse::<u128>().expect("Bad Parse"), 1))
+        .collect::<Vec<(u128, u64)>>();
 
     println!("{:?}", line);
+    println!();
+    for _ in 0..75 {
+        let mut insert_vec: VecDeque<(usize, String, u64)> = VecDeque::new();
+        for (i, n) in line.iter_mut().enumerate() {
+            let number = n.0.to_string();
+            if number.len() % 2 == 0 {
+                let middle = number.len() / 2;
+                let val: (usize, String, u64) = (
+                    i,
+                    number[middle..]
+                        .parse::<u128>()
+                        .expect("Bad parsing")
+                        .to_string(),
+                    n.1,
+                ); // to
+                   // remove leading zeros in the second value
+                n.0 = number[..middle]
+                    .parse::<u128>()
+                    .expect("Bad parse in workload");
+                insert_vec.push_back(val);
+            } else if n.0 == 0 {
+                n.0 = 1;
+            } else {
+                n.0 *= 2024;
+            }
+        }
 
-    let mut rock_count: u128 = 0;
-    for rock in line {
-        rock_count += action(rock, 0);
+        // modify the vector
+        while !insert_vec.is_empty() {
+            let (pos, val, multiplicity) = insert_vec.pop_back().expect("Pop vector bad news");
+            line.insert(
+                pos + 1,
+                (
+                    val.parse::<u128>()
+                        .expect("Parsing gone wrong during injections"),
+                    multiplicity,
+                ),
+            );
+        }
+
+        // find duplicates and shrink the vector
+        let mut remove_indices: Vec<usize> = Vec::new();
+        for i in 0..line.len() {
+            if remove_indices.contains(&i) {
+                continue;
+            }
+            for index in i + 1..line.len() {
+                if remove_indices.contains(&index) {
+                    continue;
+                }
+                if line[index].0 == line[i].0 {
+                    line[i].1 += line[index].1;
+                    remove_indices.push(index); // line[index] is now account for in multiplicity
+                }
+            }
+        }
+
+        remove_indices.sort();
+
+        while let Some(loc) = remove_indices.pop() {
+            line.remove(loc);
+        }
     }
 
-    println!("Rock Count {}", rock_count);
+    println!("{:?}", line);
+    let mut rock_count: u128 = 0;
+    for rock in line {
+        rock_count += rock.1 as u128;
+    }
+    println!("{:?}", rock_count);
 }
